f056ff47b9b5dbd5b1a1436536a933cc6eebd361
Mayank <68636590+MynkSinghal@users.noreply.github.com>
2024-07-22 00:32:00 +0530

    Updated

--- seeds.js
+++ seeds.js
@@ -10,18 +10,7 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
         console.log(err)
     })
 
-// const p = new Product({
-//     name: 'Ruby Grapefruit',
-//     price: 1.99,
-//     category: 'fruit'
-// })
-// p.save()
-//     .then(p => {
-//         console.log(p)
-//     })
-//     .catch(e => {
-//         console.log(e)
-//     })
+
 
 const seedProducts = [
     {
